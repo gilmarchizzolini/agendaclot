355cf26476708ea0697d32ad9ee06a5022298f71
gilmarchizzolini <juniorchizzolini@gmail.com>
2026-09-14 11:01:50 -0300

    Delete supabase_v8_delete_policy.sql

--- supabase_v8_delete_policy.sql
+++
@@ -1,8 +0,0 @@
--- Execute uma única vez no Supabase SQL Editor para habilitar o botão “↩ Desfazer”.
-grant delete on table public.producao to anon;
-
-create policy "Agenda CLOT - desfazer"
-on public.producao
-for delete
-to anon
-using (true);
